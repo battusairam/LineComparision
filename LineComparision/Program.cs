c0f224399fde41cc328915b1843f7a3eb4c7524d
Ganapati <ganapatasakatwa@gmail.com>
2023-05-18 06:32:37 +0530

    [GK]Calculating Length of the Line

---
+++ LineComparision/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LineComparision
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on .Net");
+            //UC1-- Calculating Length
+            int x1, x2, y1, y2;
+            double length;
+            Console.WriteLine("------------Calculating Length of the Line-------------");
+            Console.WriteLine("Enter X1 Value");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y1 Value");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X2 Value");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y2 Value");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of the Line is--"+length);
+
+        }
+    }
+}
